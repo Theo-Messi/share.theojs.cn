70101097b57fc5c68d93fb9019306513ca1c400f
Theo <fanxiaobin422@gmail.com>
2024-08-31 21:09:06 +0800

    chore: The event has expired

--- .vitepress/data/AsideData.ts
+++ .vitepress/data/AsideData.ts
@@ -6,19 +6,33 @@ export const Aside_Data = [
   //   info1: '月付 85折 优惠码  2461885',
   //   info2: '年付 64折 优惠码  2461880'
   // },
-  {
-    link: 'https://ivt01.qytaff.cc/register?aff=jjgD79Jd',
-    icon: 'https://www.qingyunti.cc/images/qytlogo-144-49.png',
-    Activity: '青云梯·三周年专属优惠',
-    info1:
-      '月付 85折 优惠码 <span style="color: var(--vp-c-brand-3)">qyt85</span>',
-    info2:
-      '季付以上8折 优惠码 <span style="color: var(--vp-c-brand-3)">qyt8</span>'
-  },
+  // {
+  //   link: 'https://ivt01.qytaff.cc/register?aff=jjgD79Jd',
+  //   icon: 'https://www.qingyunti.cc/images/qytlogo-144-49.png',
+  //   Activity: '青云梯·三周年专属优惠',
+  //   info1:
+  //     '月付 85折 优惠码 <span style="color: var(--vp-c-brand-3)">qyt85</span>',
+  //   info2:
+  //     '季付以上8折 优惠码 <span style="color: var(--vp-c-brand-3)">qyt8</span>'
+  // },
   {
     link: 'https://pages.aliyundrive.com/mobile-page/web/commission.html?disableNav=YES',
     icon: 'https://img2.baidu.com/it/u=3966973686,63129941&fm=253&fmt=auto&app=138&f=PNG?w=500&h=500',
     Activity: '阿里云盘分享得现金活动',
     info1: '单笔返佣最高<span style="color: var(--vp-c-brand-3)"> 59 </span>元'
+  },
+  {
+    link: 'https://github.com/Theo-Messi/tools/tree/main/packages/Lumen',
+    icon: 'https://tools.theojs.cn/TM-FELogo.png',
+    name: '@theojs/Lumen',
+    info1: '集成 Vue 功能组件',
+    info2: 'VitePress 主题美化'
+  },
+  {
+    link: 'https://github.com/Theo-Messi/tools/tree/main/packages/Solis',
+    icon: 'https://i.theojs.cn/docs/202405101119004.png',
+    name: '@theojs/Solis',
+    // hide1: 'VitePress 的博客主题',
+    info2: 'VitePress 的博客主题'
   }
 ]
